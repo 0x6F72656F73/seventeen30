f4f8c2f75211f3e7cc9abe21e2114c0b3aeceb36
0x6F72656F73 <godbobz12@gmail.com>
2023-07-22 17:39:15 -0700

    feat: update form UI

--- src/components/WorkoutForm.tsx
+++ src/components/WorkoutForm.tsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import { Anton, Libre_Baskerville } from 'next/font/google'
- 
+import {IColorVariants} from '@/types/common'
+import { motion } from 'framer-motion';
+
 const libreBaskerville = Libre_Baskerville({ weight: '400', subsets: ['latin'] })
 
-import {IColorVariants} from '@/types/common'
+
 
 
 interface SectionProps {
@@ -41,28 +43,41 @@ const Section = ({ title, color, fields, onFieldValueChange }: SectionProps) => 
 
 
   return (
-    <div className='text-center text-white font-bold '>
-      <div className={`flex items-center justify-center w-[50vh] h-[10vh] p-4 ${getColorClassNames(color)[0]} text-3xl`}>
-        {title}
-      </div>
-      <div className="w-[50vh] my-4">
-        {fields.map((field) => (
-          <div
+      <div className='text-center text-white font-bold'>
+        <div className={`flex items-center justify-center w-[50vh] h-[10vh] p-4 ${getColorClassNames(color)[0]} text-3xl`}>
+          {title}
+        </div>
+        <div className="w-[50vh] h-[10vh] my-4 text-xl">
+          {fields.map((field) => (
+             <motion.div
             key={field}
             onClick={() => handleFieldValueChange(field)}
-            className={`cursor-pointer p-2 mb-2 border-2 ${getColorClassNames(color)[1]}`}
-          >
-            {field}
-          </div>
-        ))}
+            className={`cursor-pointer p-2 mb-2 border-8 ${getColorClassNames(color)[1]}`}
+            whileHover={{ scale: 1.1 }} // Animation when hovering over the field
+            whileTap={{ scale: 0.9 }} // Animation when the field is clicked
+            // animate={selectedField === field ? { x:100 } : {}} // Animation when a field is selected
+            transition={selectedField === field ? { ease: "linear",
+            duration: 2,
+            x: { duration: 1 }} : {}}
+           >
+             {field}
+           </motion.div>
+            // <div
+            //   key={field}
+            //   onClick={() => handleFieldValueChange(field)}
+            //   className={`cursor-pointer p-2 mb-2 border-8 ${getColorClassNames(color)[1]}`}
+            // >
+            //   {field}
+            // </div>
+          ))}
+        </div>
       </div>
-    </div>
   );
 };
 
 const WorkoutForm = () => {
   const [formData, setFormData] = useState({
-    totalTime: '',
+    span: '',
     amount: '',
     level: '',
     duration: '',
@@ -84,45 +99,51 @@ const WorkoutForm = () => {
   };
 
   return (
-    <div className={`grid grid-cols-2 justify-items-center gap-y-[5vh] mt-[5vh] ${libreBaskerville.className}`}>
-      <Section
-        title="TIME"
-        color="bright-pink"
-        fields={['1 day plan', '7 day plan', '30 day plan']}
-        onFieldValueChange={handleFieldValueChange}
-      />
-      <Section
-        title="AMOUNT"
-        color="bright-green"
-        fields={['3 Workouts', '4 Workouts', '5 Workouts']}
-        onFieldValueChange={handleFieldValueChange}
-      />
-      <Section
-        title="LEVEL"
-        color="bright-orange"
-        fields={['Beginner', 'Intermediate','Advanced']}
-        onFieldValueChange={handleFieldValueChange}
-      />
-      <Section
-        title="TIME"
-        color="bright-blue"
-        fields={['15 Minutes', '30 Minutes', '60 Minutes']}
-        onFieldValueChange={handleFieldValueChange}
-      />
-      <Section
-        title="TYPE"
-        color="bright-yellow"
-        fields={['Cardio', 'Weight Training', 'Calisthenics']}
-        onFieldValueChange={handleFieldValueChange}
-      />
-      <Section
-        title="SPORT"
-        color="bright-blue-2"
-        fields={['Basketball', 'Soccer', 'Football']}
-        onFieldValueChange={handleFieldValueChange}
-      />
-      <button onClick={handleSubmit} className="bg-blue-500 text-white px-4 py-2 rounded mt-4">
-        Submit
+    <div className="flex flex-col">
+      <div className={`grid md:grid-cols-2 sm:grid-cols-1 justify-items-center gap-y-[20vh] mt-[30vh] ${libreBaskerville.className}`}>
+        <Section
+          title="SPAN"
+          color="bright-pink"
+          fields={['1 Day Plan', '7 Day Plan', '30 Day Plan']}
+          onFieldValueChange={handleFieldValueChange}
+        />
+        <Section
+          title="AMOUNT"
+          color="bright-green"
+          fields={['3 Workouts', '4 Workouts', '5 Workouts']}
+          onFieldValueChange={handleFieldValueChange}
+        />
+        <Section
+          title="LEVEL"
+          color="bright-orange"
+          fields={['Beginner', 'Intermediate','Advanced']}
+          onFieldValueChange={handleFieldValueChange}
+        />
+        <Section
+          title="DURATION"
+          color="bright-blue"
+          fields={['15 Minutes', '30 Minutes', '60 Minutes']}
+          onFieldValueChange={handleFieldValueChange}
+        />
+        <Section
+          title="TYPE"
+          color="bright-yellow"
+          fields={['Cardio', 'Weight Training', 'Calisthenics']}
+          onFieldValueChange={handleFieldValueChange}
+        />
+        <Section
+          title="SPORT"
+          color="bright-blue-2"
+          fields={['Basketball', 'Soccer', 'Football']}
+          onFieldValueChange={handleFieldValueChange}
+        />
+      </div>
+      <button onClick={handleSubmit} className="flex items-center mt-[30vh] mx-auto">
+      <motion.div whileTap={{ scale: 0.9 }}>
+        <div className="text-9xl hollow-text-3 text-center"> 
+          DONE
+        </div>
+      </motion.div>
       </button>
     </div>
   );
