9f3aaf3587fe5b906dee3e91595a3de70ab05da0
0x6F72656F73 <godbobz12@gmail.com>
2023-07-23 21:36:11 -0700

    fix: UI Responsive Design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
     <main>
       <div className="min-h-screen flex flex-col items-center justify-center">
         <h1 className='text-9xl text-bright-green'>S E V E N T E E N</h1>
-        <h1 className='text-9xl -mt-[4%] text-bright-pink'>3 0</h1>
+        <h1 className='text-9xl md:-mt-[8vh] lg:-mt-[4vh] text-bright-pink'>3 0</h1>
 
         <div className='min-w-[85%] min-h-[10vh] bg-bright-pink mt-[8%]'>
           <div className='py-[5vh] text-center text-5xl hollow-text'>
