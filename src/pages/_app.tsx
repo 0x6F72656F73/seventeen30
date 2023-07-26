6253ba6eef1ea90b8ecb2991c3b4087c71922351
0x6F72656F73 <godbobz12@gmail.com>
2023-07-25 20:49:58 -0700

    repo: add Vercel Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,17 @@
-import '../styles/globals.css'
+import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-// import { Anton } from 'next/font/google'
+import { Anton } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
 
-// const anton = Anton({ weight: '400', subsets: ['latin'] })
+const anton = Anton({ weight: '400', subsets: ['latin'] })
 
 // import { AnimatePresence } from 'framer-motion';
 
 export default function App({ Component, pageProps, }: AppProps) {
   return (
-    // <main className={anton.className}>
-    <main>
+    <main className={anton.className}>
       <Component {...pageProps} />
+      <Analytics />
     </main>
   )
 }
