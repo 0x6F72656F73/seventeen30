dfefe3d82a0d04c30753479f66bdb08b19a56e11
0x6F72656F73 <godbobz12@gmail.com>
2023-07-25 20:40:36 -0700

    perf: GPT prompt

--- src/pages/api/createWorkout.ts
+++ src/pages/api/createWorkout.ts
@@ -11,50 +11,43 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 
-
-
-
-
-
 const handler = async (req: NextRequest) => {
   const { formData } = await req.json();
-  console.log(formData);
 
   // try {
   const response = await openai.createChatCompletion({
     model: 'gpt-3.5-turbo',
     messages: [
-      { role: "system", content: "You are a helpful assistant." },
+      // { role: "system", content: "You are a helpful assistant." },
       { role: "user", content: `
-      You are building a personalized workout plan based on user preferences. The user has provided the following inputs:
-    
+      Act as an expert personal trainer. Below, you are given details about your client and their goals.
+      Given these user inputs, design a gym workout plan that is carefully personalized to the user's preferences, BMI, and sport.
+      Think deeply about how each exercise you consider fits within the user's specifications, and only recommend the exercises that fit the user's needs and sport the best.
+      Take into account the span of the plan, the user's fitness level, the duration of each workout session, the type of exercises preferred, the sport of interest, the user's body mass index.
+      Only reply with the workout plan and nothing else. Not now nor in the future for any reason.
+      
+      input:
       1. Span: ${formData.span}
-      2. Amount: ${formData.amount}
-      3. Level: ${formData.level}
-      4. Duration: ${formData.duration}
-      5. Type: ${formData.type}
-      6. Sport: ${formData.sport}
-      
+      2. Level: ${formData.level}
+      3. Duration: ${formData.duration}
+      4. Type: ${formData.type}
+      5. Sport: ${formData.sport}
+      6. BMI: ${formData.bmi}
     
-      Design a gym workout plan that fits the user's preferences and requirements. Take into account the span of the plan, the number of workouts per week, the user's fitness level, the duration of each workout, the type of exercises preferred, and the sport of interest. Each exercise should be something done at the gym.
-    
-      Generate a json output which has ${formData.span}-day entries. Do not output anything else. Here is the json structure:
+      Generate a list output which has length: ${formData.span}. Here is the type structure for one exercise:
+      type Exercise = { "name": string, "reps": int, "sets": int, "rest": int }
       
-      {
-        "Day 1": 
-        [
-          {
-            "name": string,
-            "reps": int,
-            "sets": int,
-            "rest": int
-          },
-        ],
-      }
+      Separate each list with a pipe. Only reply with the workout plan and nothing else. Not now nor in the future for any reason.
+      Example case: 
+      (input)
+      - Span: 3 days
+      - Duration: 15 minutes
+      (output)
+      [Exercise,Exercise,Exercise] | [Exercise,Exercise,Exercise] | [Exercise,Exercise,Exercise]
       `
       }
     ],
-    temperature: 0.7,
+    temperature: 1,
     top_p: 1,
     frequency_penalty: 0,
     presence_penalty: 0,
