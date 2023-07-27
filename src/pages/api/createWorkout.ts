fa74ada469ab81f35eba8c7967e8c8c8e6c51e41
0x6F72656F73 <godbobz12@gmail.com>
2023-07-26 19:01:33 -0700

    perf: GPT Prompt

--- src/pages/api/createWorkout.ts
+++ src/pages/api/createWorkout.ts
@@ -21,34 +21,37 @@ const handler = async (req: NextRequest) => {
     messages: [
       // { role: "system", content: "You are a helpful assistant." },
       { role: "user", content: `
-      Act as an expert personal trainer. Below, you are given details about your client and their goals.
-      Given these user inputs, design a gym workout plan that is carefully personalized to the user's preferences, BMI, and sport.
-      Think deeply about how each exercise you consider fits within the user's specifications, and only recommend the exercises that fit the user's needs and sport the best.
-      Take into account the span of the plan, the user's fitness level, the duration of each workout session, the type of exercises preferred, the sport of interest, the user's body mass index.
-      Only reply with the workout plan and nothing else. Not now nor in the future for any reason.
+      Act as an expert personal trainer. Below, you are given details about your client and their goals. ONLY REPLY WITH THE LIST-FORMATTED WORKOUT PLAN AND NOTHING ELSE. NOT NOW NOR IN THE FUTURE FOR ANY REASON.
       
-      input:
-      1. Span: ${formData.span}
+      Input:
+      1. Days: ${formData.span}
       2. Level: ${formData.level}
       3. Duration: ${formData.duration}
       4. Type: ${formData.type}
       5. Sport: ${formData.sport}
       6. BMI: ${formData.bmi}
-    
-      Generate a list output which has length: ${formData.span}. Here is the type structure for one exercise:
-      type Exercise = { "name": string, "reps": int, "sets": int, "rest": int }
+
+      Given these user inputs, design a gym workout plan that is carefully personalized, taking into account the number of days, the duration of each workout session, the user's fitness level, the type of exercises preferred, the sport of interest, the user's body mass index.
+      Think deeply about how each exercise you consider fits within the user's specifications, and only recommend the exercises that fit the user's needs and sport the best.
+      Each exercise cannot be longer than 25 characters. The number of days is not related to the duration of each workout session.
+      ONLY REPLY WITH THE LIST-FORMATTED WORKOUT PLAN AND NOTHING ELSE. NOT NOW NOR IN THE FUTURE FOR ANY REASON.
       
-      Separate each list with a pipe. Only reply with the workout plan and nothing else. Not now nor in the future for any reason.
-      Example case (note Exercise denotes the type above, not the actual exercise): 
-      (input)
+      
+      
+      Generate a list-formatted output which has length: ${formData.span}. Here is the type structure for one exercise:
+      type $Exercise = { "name": string, "reps": int, "sets": int, "rest": int }
+      
+      Separate each list with a pipe and space on both sides. ONLY REPLY WITH THE LIST-FORMATTED WORKOUT PLAN AND NOTHING ELSE. NOT NOW NOR IN THE FUTURE FOR ANY REASON.
+      Example case (note $Exercise denotes the type above, not the actual exercise. DO NOT OUTPUT [$Exercise,$Exercise,Exercise]): 
+      (example input)
       - Span: 3 days
       - Duration: 15 minutes
-      (output)
-      [Exercise,Exercise,Exercise] | [Exercise,Exercise,Exercise] | [Exercise,Exercise,Exercise]
+      (example output)
+      [Exercise 1, Exercise 2, Exercise 3] | [Exercise 1, Exercise 2, Exercise 3] | [Exercise 1, Exercise 2, Exercise 3]
       `
       }
     ],
-    temperature: 1,
+    temperature: .7,
     top_p: 1,
     frequency_penalty: 0,
     presence_penalty: 0,
