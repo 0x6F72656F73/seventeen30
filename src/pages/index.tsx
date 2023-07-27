97a73e4f166971d40b649c8eb30e87db6460b49a
0x6F72656F73 <godbobz12@gmail.com>
2023-07-27 16:53:41 -0700

    perf: hero SVG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
   return (
     <main>
       <div className="min-h-screen flex flex-col items-center justify-center">
-        <Image src="/images/Hero.png" alt="Hero" priority={true} width="0" height="0" sizes="100vw" className="w-full h-auto" placeholder="blur" blurDataURL="images/Hero.png"/>
+        <Image src="images/seventeen30.svg" alt="Hero" priority={true} width="0" height="0" sizes="100vw" className="w-full h-auto" placeholder="blur" blurDataURL="images/seventeen30.svg"/>
 
         <div className='min-w-[85%] min-h-[10vh] bg-bright-pink mt-[6%]'>
           <div className={`py-[5vh] hollow-text text-center text-6xl ${anton.className}`}>
