c6fd07760a645dfea9435a33e33756f89491252d
0x6F72656F73 <godbobz12@gmail.com>
2023-07-26 19:02:01 -0700

    repo: cleanup

--- src/scripts/testApi.ts
+++ src/scripts/testApi.ts
@@ -1,10 +1,11 @@
 const formData = {
-    span: '2 Day Plan',
+    span: '8 Day Plan',
     level: 'Beginner',
-    duration: '30 Minutes',
+    duration: '15 Minutes',
     type: 'Strength Training',
     sport: 'Basketball',
-    bmi: '15%',
+    height: '65',
+    weight: '120',
 }
 
 
@@ -27,65 +28,29 @@ async function testApi() {
         let allData = '';
         while (true) {
             const {value, done} = await reader.read();
-            // console.log(value, value?.length);
             if (done) break;
             allData += value;
-            // if (value.startsWith("DAY")) {
-            //     console.log("NEW DAY",value);
-            // }
+
             if (value.includes("]")) {
+                allData = allData.replace('|', '');
                 console.log("NEW DAY");
-                console.log(allData);
+                try {
+                const x = JSON.parse(allData);
+                console.log(x,x.length);
+                } catch (SyntaxError) {
+                    console.log("Syntax Error:");
+                    console.log(allData);
+                }
+                allData = '';
             }
         }
         
         console.log('Response fully received');
         console.log(allData);
-
-        // const data = response.body;
-        // if (!data) {
-        //     return;
-        // }
-
-        // const reader = data.getReader();
-        // const decoder = new TextDecoder();
-        // let done = false;
-
-        // while (!done) {
-        //     const { value, done: doneReading } = await reader.read();
-        //     done = doneReading;
-        //     const chunkValue = decoder.decode(value);
-        //     const cleanedJsonString = chunkValue.substring("data: ".length);
-        //     console.log(cleanedJsonString);
-        //     const jsonData = JSON.parse(cleanedJsonString);
-        //     console.log(jsonData.choices[0].delta.content);
-        // }
-
-
-        // for await (const chunk of response.body) {
-        //     console.log(chunk.toString())
-        // };
     } catch (error) {
         console.log(error);
     }
-    // const data = await response.json();
-    // console.log(data);
-    // const fitness = data.fitnessData;
-    // console.log(fitness);
 
-    // const days = data.workoutDays;
-    // console.log(days);
-
-    // for (const day in days) {
-    //     console.log(`Day: ${day}`);
-        
-    //     // Loop through the exercises of each day
-    //     for (const exercise of days[day].exercises) {
-    //       console.log(exercise);
-    //     }
-        
-    //     console.log(); // Add an empty line between days for readability
-    //   }
 
 }
 
