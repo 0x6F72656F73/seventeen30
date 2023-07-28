b9c10be34355781cd7abfcffa97656a7de540882
0x6F72656F73 <godbobz12@gmail.com>
2023-07-28 07:02:53 -0700

    perf: GPT prompt

--- src/pages/api/createWorkout.ts
+++ src/pages/api/createWorkout.ts
@@ -1,8 +1,7 @@
 
-import type { NextApiRequest, NextApiResponse } from 'next';
 import { Configuration, OpenAIApi } from "openai-edge";
 import { OpenAIStream, StreamingTextResponse } from 'ai';
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest } from 'next/server';
 
 
 const configuration = new Configuration({
@@ -33,12 +32,13 @@ const handler = async (req: NextRequest) => {
 
       Given these user inputs, design a gym workout plan that is carefully personalized, taking into account the number of days, the duration of each workout session, the user's fitness level, the type of exercises preferred, the sport of interest, the user's body mass index.
       Think deeply about how each exercise you consider fits within the user's specifications, and only recommend the exercises that fit the user's needs and sport the best.
-      Each exercise cannot be longer than 25 characters. The number of days is not related to the duration of each workout session.
+      The number of days is not related to the duration of each workout session. Try to make no workout session the exact same, but it is okay to repeat exercises after at least 3 days.
       ONLY REPLY WITH THE LIST-FORMATTED WORKOUT PLAN AND NOTHING ELSE. NOT NOW NOR IN THE FUTURE FOR ANY REASON.
       
       Generate a list-formatted output which has length: ${formData.span}. If reps is a more accurate measurement than duration, set the duration to 0. Vice versa. The units for duration is minutes.
       Here is the type structure for one exercise:
       type $Exercise = { "name": string, "reps": int, "duration": int, "sets": int, "rest": int }
+      required constraint: name.length < 20
       
       Separate each list with a pipe and space on both sides. ONLY REPLY WITH THE LIST-FORMATTED WORKOUT PLAN AND NOTHING ELSE. NOT NOW NOR IN THE FUTURE FOR ANY REASON.
       Example case (note $Exercise denotes the type above, not the actual exercise. DO NOT OUTPUT [$Exercise,$Exercise,Exercise]): 
@@ -50,11 +50,10 @@ const handler = async (req: NextRequest) => {
       `
       }
     ],
-    temperature: .7,
+    temperature: 0.7,
     top_p: 1,
     frequency_penalty: 0,
     presence_penalty: 0,
-    max_tokens: 1500,
     stream: true,
   });
 
